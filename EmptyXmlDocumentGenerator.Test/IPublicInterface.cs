a70e96f7a33e3adbfd639377a07f9e19d56e990a
Tana <finaltruth.shintai@gmail.com>
2020-12-07 23:51:42 +0900

    検証用プロジェクトを追加。

---
+++ EmptyXmlDocumentGenerator.Test/IPublicInterface.cs
@@ -0,0 +1,23 @@
+﻿namespace EmptyXmlDocumentGenerator.Test
+{
+    /// <summary>
+    /// public interface PublicInterface
+    /// </summary>
+    public interface IPublicInterface
+    {
+        /// <summary>static int field</summary>
+        static int field;
+
+        /// <summary>
+        /// int Property { get; set; }
+        /// </summary>
+        int Property { get; set; }
+
+        /// <summary>
+        /// int Method(int arg1)
+        /// </summary>
+        /// <param name="arg1">int arg1</param>
+        /// <returns>returns int</returns>
+        int Method(int arg1);
+    }
+}
