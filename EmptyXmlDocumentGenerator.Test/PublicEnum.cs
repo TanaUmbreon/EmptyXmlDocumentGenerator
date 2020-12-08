74538043d006de880274dbab92d2fa20e08125e3
Tana <finaltruth.shintai@gmail.com>
2020-12-08 22:51:22 +0900

    検証用データに列挙型を追加。

---
+++ EmptyXmlDocumentGenerator.Test/PublicEnum.cs
@@ -0,0 +1,15 @@
+﻿namespace EmptyXmlDocumentGenerator.Test
+{
+    /// <summary>
+    /// public enum PublicEnum
+    /// </summary>
+    public enum PublicEnum
+    {
+        /// <summary>Value1</summary>
+        Value1,
+        /// <summary>Value2</summary>
+        Value2,
+        /// <summary>Value3</summary>
+        Value3,
+    }
+}
